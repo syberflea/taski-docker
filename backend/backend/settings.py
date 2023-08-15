da496a3734957298a0f83069c9f1abdf621ea455
syberflea <syberlea@gmail.com>
2023-08-15 15:11:24 +0300

    Localy deployed forked repo.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,18 @@
+import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
