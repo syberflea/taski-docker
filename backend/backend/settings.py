4c028042af48e2f5f60c45b83d25637a23c40243
syberflea <syberlea@gmail.com>
2023-08-16 08:39:59 +0300

    Add Dockerfiles for both parts of the project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,11 +63,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+# BASE_DIR / 'db.sqlite3'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
