a8eec40cf1c76697fd4847bc309091a4c01bc25b
syberflea <syberlea@gmail.com>
2023-08-16 14:29:32 +0300

    Switched to Postgres SQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,8 +66,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # BASE_DIR / 'db.sqlite3'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/db.sqlite3',
+        # Меняем настройку Django: теперь для работы будет использоваться
+        # бэкенд postgresql
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
