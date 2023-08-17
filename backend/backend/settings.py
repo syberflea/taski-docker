6af41540c20c320bf87d2958c750b242b4bcb43d
syberflea <syberlea@gmail.com>
2023-08-17 15:32:12 +0300

    Add test 003

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,16 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+DEBUG = False
+# load_dotenv()
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
-load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-DEBUG = os.getenv('DEBUG')
+# DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
